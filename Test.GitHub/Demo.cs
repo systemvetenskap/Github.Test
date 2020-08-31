0f114d059e4ba2b7e2c4a557d8b3f32c6088cb82
Erik Öberg <erik.oberg@miun.se>
2020-08-31 10:48:47 +0200

    skapat metoden  test2

---
+++ Test.GitHub/Demo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test.GitHub
+{
+    class Demo
+    {
+        public void Test2()
+        {
+            //
+        }
+    }
+}
